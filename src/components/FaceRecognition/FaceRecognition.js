2902473a320873afdfa0fc31496cfe8f1b3e579a
Vitor Rodrigues Costa <vitorc1206@gmail.com>
2018-07-04 21:22:59 -0300

    Adding experimental validation of form

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './FaceRecognition.css'
 
-const FaceRecognition = ({imageURL, box}) => {
-    return (
+const showPhoto = (imageURL, box) => {
+    if(imageURL){
+        return(
         <div className='center'>
             <div className='photoContainer'>
                 <img id='inputimage' className='photo' src={imageURL} alt='Recognized Faces'/>  
                 <div className='bounding-box' style={{top: box.topRow, left: box.leftCol, bottom: box.bottomRow, right: box.rightCol }}></div>
             </div>
         </div>
-    );
+        );
+    }
+    else{
+        return null;
+    }
+}
+
+const FaceRecognition = ({imageURL, box}) => {
+    return showPhoto(imageURL, box);
 }
 
 export default FaceRecognition;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,31 +5,120 @@ class Register extends React.Component {
         super(props);
         this.state = {
             name : '',
+            namefeedback: '',
             email : '',
+            emailfeedback: '',
             password: '',
+            passwordfeedback: '',
+            valid: false
+        }
+        this.references = React.createRef();
+    }
+
+
+    onNameChange = (event) => {
+        const {value} = event.target;
+
+        this.setState({ name : value});
+
+        if(!value){
+            this.setState({
+                namefeedback : 'You have a name...rigth?',
+                valid: false
+            });
+        }
+        else if(value.length > 100){
+            this.setState({
+                namefeedback : 'Your name need to be less than 100 characteres long',
+                valid : false
+            });
+        }
+        else{
+            this.setState({
+                namefeedback : '',
+            });            
         }
     }
 
-    onNameChange = (event) => {
-        this.setState({ name : event.target.value});
-    }
-
     onEmailChange = (event) => {
-        this.setState({ email : event.target.value});
+        const {value} = event.target;
+        const re = new RegExp('^\\S+@\\S+[\\.][0-9a-z]+$');
+
+        this.setState({email : value});        
+
+        console.log(value + " is " + re.test(value));
+
+        if(!value){
+            this.setState({
+                emailfeedback : 'You need a email so that you can login later',
+                valid: false
+            });
+        }
+        else if(!re.test(value)){
+            this.setState({
+                emailfeedback : `That's not a valid email format`,
+                valid : false
+            });
+        }
+        else{
+            this.setState({
+                emailfeedback : '',
+            });            
+        }
     }
 
     onPasswordChange = (event) => {
-        this.setState({ password : event.target.value});
+        const {value} = event.target;
+
+        this.setState({ password : value});
+
+        if(!value){
+            this.setState({
+                passwordfeedback : 'You need a password so that you can login later',
+                valid: false
+            });
+        }
+        else{
+            this.setState({
+                passwordfeedback : '',
+            });            
+        }
+    }
+
+    checkValidEmail = () => {
+        const {email} = this.state;
+        fetch('https://young-wave-95662.herokuapp.com/registercheck', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({
+                email : email
+            }),
+        }).then( response => {
+            if(response.status === 200){
+                return true;
+            }
+            else if(response.status === 409){
+                this.setState({emailfeedback : 'Esse email já está registrado'})
+                return false;
+            }
+        })
     }
 
     onSubmitRegister = () =>{
+        const {name, email, password, valid} = this.state;
+        
+        //If any of the fields is invalid or the email is not valid
+        if(!valid || this.checkValidEmail()) {return;}
+
+        return;
+        /*
         fetch('https://young-wave-95662.herokuapp.com/register', {
             method: 'POST',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify({
-                name : this.state.name,
-                password : this.state.password,
-                email : this.state.email
+                name : name,
+                password : password,
+                email : email
             }),
         })
         .then(response => response.json())
@@ -38,7 +127,11 @@ class Register extends React.Component {
                 this.props.loadUser(user);
                 this.props.onRouteChange('home');
             }
+            else{
+
+            }
         });
+        */
     }
 
     render(){    
@@ -51,14 +144,17 @@ class Register extends React.Component {
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="nickname">Nickname</label>
                                 <input onChange={this.onNameChange} className="pa2 input-reset ba bg-transparent hover-bg-blue  hover-white w-100" type="text" name="nickname"  id="nickname"/>
+                                <label className="db fw6 lh-copy f6 dark-red" htmlFor="nickname">{this.state.namefeedback}</label>
                             </div>
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                                 <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-blue  hover-white w-100" type="email" name="email-address"  id="email-address"/>
+                                <label className="db fw6 lh-copy f6 dark-red" htmlFor="email-address">{this.state.emailfeedback}</label>
                             </div>
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                                 <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-blue hover-white w-100" type="password" name="password"  id="password"/>
+                                <label className="db fw6 lh-copy f6 dark-red" htmlFor="password">{this.state.passwordfeedback}</label>
                             </div>
                         </fieldset>
                         <div className="">
